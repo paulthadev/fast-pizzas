0494ce607d9027c6266627d6309905eb4d7759b1
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-13 10:27:16 +0100

    style(file): :art: setup professional file structure

---
+++ src/features/cart/CartItem.jsx
@@ -0,0 +1,18 @@
+import { formatCurrency } from "../../utils/helpers";
+
+function CartItem({ item }) {
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  return (
+    <li>
+      <p>
+        {quantity}&times; {name}
+      </p>
+      <div>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default CartItem;
