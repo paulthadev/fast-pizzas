424035f99759285e458e970d296c9572bb344e10
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-15 00:33:18 +0100

    feat: :sparkles: add search orders query and fetch order implementation

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+
     children: [
       {
         path: "/",
@@ -32,6 +33,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza Co</Link>
+
+      <SearchOrder />
     </header>
   );
 }
