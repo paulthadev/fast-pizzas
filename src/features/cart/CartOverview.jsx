19551ed74f1dbf09c73658b5cd9ff6db2e7b0d8c
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-14 05:41:24 +0100

    feat: :art: fetch data with React Loader for the pizza menu

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -4,7 +4,7 @@ function CartOverview() {
   return (
     <div>
       <p>
-        <span>23 pizzas</span>
+        <span>23 pizzas </span>
         <span>$23.45</span>
       </p>
       <Link to="/Cart">Open cart &rarr;</Link>
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+// loader function from API
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
