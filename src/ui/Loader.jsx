204ba3b26e1015e5f5bc8b0fedfbaddda547b315
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-27 01:41:22 +0100

    style: :lipstick: style the loaderç absolute positionning

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,9 @@
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-300/50 backdrop-blur-sm">
+      <div className="loader"></div>;
+    </div>
+  );
 }
 
 export default Loader;
