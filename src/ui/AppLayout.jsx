bfe0e4392b50dfa7258f073c118808fa5d9fbe3d
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-27 21:38:57 +0100

    refactor: :zap: update the ui app layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,9 +9,9 @@ function AppLayout() {
 
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      <Header />
-
       {isLoading && <Loader />}
+
+      <Header />
 
       <div className="overflow-scroll">
         <main className="mx-auto max-w-3xl">
