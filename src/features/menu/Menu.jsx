7a396fc9dfab61a27366a58e13eca05666989d9a
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-28 00:02:16 +0100

    style: :lipstick: styling the menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -12,7 +12,7 @@ function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul>
+    <ul className="divide-y divide-stone-300 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
