5f5e4a6fa053042f5506a72758a3f2405b92e40f
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-27 02:31:58 +0100

    refactor: :wrench: Add font to configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
     extend: {},
   },
   plugins: [],
