b7c3bfe177dbc58e218ba514e06a2c069b472bae
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-28 00:06:33 +0100

    style: :lipstick: styling the menu

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -11,7 +11,7 @@ function MenuItem({ pizza }) {
         alt={name}
         className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
       />
-      <div className="flex grow flex-col">
+      <div className="flex grow flex-col py-1">
         <p className="font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">
           {ingredients.join(", ")}
