ca45a835511552347f4ead5b895356078700f029
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-28 00:02:57 +0100

    style: remove typogramatical errors

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,7 +1,7 @@
 function Loader() {
   return (
     <div className="absolute inset-0 flex items-center justify-center bg-slate-300/50 backdrop-blur-sm">
-      <div className="loader"></div>;
+      <div className="loader"></div>
     </div>
   );
 }
