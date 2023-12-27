5e82410324dd2f10b6039e376a32c76f344e2569
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-27 23:56:21 +0100

    refactor: :sparkles: button component more versatile
    It receives a prop type (primary or small),creating different styles for each types

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "focus-bg-yellow-300 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+function Button({ children, disabled, to, type }) {
+  const base =
+    "focus-bg-yellow-300 inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4 ",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+  };
 
   if (to)
     return (
-      <Link className={className} to={to}>
+      <Link className={styles[type]} to={to}>
         {children}
       </Link>
     );
 
   return (
-    <button className={className} disabled={disabled}>
+    <button className={styles[type]} disabled={disabled}>
       {children}
     </button>
   );
