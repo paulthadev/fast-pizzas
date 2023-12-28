756f3a036b3a4649fe0930f59d9d6bca03e2ae60
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-28 18:26:42 +0100

    feat: :art: model the user state with RTK

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,4 +1,4 @@
-function getPosition() {
+/*  function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
@@ -15,7 +15,27 @@ async function fetchAddress() {
   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
   const addressObj = await getAddress(position);
   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
-
+  
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
-}
+} */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  username: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
