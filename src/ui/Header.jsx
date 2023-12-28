e8b92786ccfefbdcafd52f2feb1c19cf3fb061b3
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-28 06:57:23 +0100

    style: :pencil2: title change

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3  uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        Fast React Pizza Co
+        Fast Pizzas
       </Link>
 
       <SearchOrder />
