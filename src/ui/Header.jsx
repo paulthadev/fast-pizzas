906d87a59dd5e769159dcf8759d51ec5ab54cd08
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-28 16:04:05 +0100

    style: :lipstick: style the order overview

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,8 +5,8 @@ import Username from "../features/user/Username";
 function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3  uppercase sm:px-6">
-      <Link to="/" className="tracking-widest">
-        Fast Pizzas
+      <Link to="/" className="font-extrabold tracking-widest">
+        Fast Pizzas 🍕
       </Link>
 
       <SearchOrder />
