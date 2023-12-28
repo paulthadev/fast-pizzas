130dca4d94e179f41e6922bf9c2a171ef817dbce
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-28 10:13:01 +0100

    refactor: :zap: customize an extra button type

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4 ",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "focus-bg-stone-300 focus:text-stone-800 inline-block rounded-full border-2 border-stone-300 px-4 py-2 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-700 focus:outline-none focus:ring focus:ring-stone-800 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-3",
   };
 
   if (to)
