50c0fffbeca44e1451a32f9f0a06860a2556077a
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-29 20:59:31 +0100

    chore: :recycle: extend button component capability
    add an ability to receive an extra type styling

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,6 +7,7 @@ function Button({ children, disabled, to, type, onClick }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4 ",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    round: base + " px-3 py-1 md:px-3.5 md:py-2 text-sm",
     secondary:
       "text-sm focus-bg-stone-300 focus:text-stone-800 inline-block rounded-full border-2 border-stone-300 px-4 py-2 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-700 focus:outline-none focus:ring focus:ring-stone-800 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-3",
   };
