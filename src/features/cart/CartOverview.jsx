fa796cfd9dbcb0aa25fc6689cdd7b16ecb8618cf
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-29 04:19:42 +0100

    chore: :construction: build cart overview with redux selectors

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,19 @@
-import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
+
   return (
     <div className=" flex items-center justify-between bg-stone-800 p-4 text-sm uppercase text-stone-200 sm:p-6 md:text-base">
       <p className=" space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas </span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas </span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
 
       <Link to="/Cart">Open cart &rarr;</Link>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,16 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  //   cart: [],
-  cart: [
-    {
-      pizzaId: 12,
-      name: "Mediterranean",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
 };
 
 const cartSlice = createSlice({
@@ -58,3 +49,13 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+// Selector functions
+
+export function getTotalCartQuantity(state) {
+  return state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+}
+
+export function getTotalCartPrice(state) {
+  return state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+}
