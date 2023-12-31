022bbbb436549a6205e2f8847a95496baf62a7e8
Paul Fadayo <paulfadayo@yahoo.com>
2023-12-31 20:16:50 +0100

    refactor: :lipstick: update ui changes to order items

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -4,7 +4,7 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li className="py-3">
+    <li className="space-y-1 py-3">
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
