3a134321944aa9bb3da2dc38af3e016361d2f178
Paul Fadayo <paulfadayo@yahoo.com>
2024-01-02 04:27:15 +0100

    feat: :sparkles: update order Priority
    updating data logic without Navigation

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -7,6 +7,7 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 /* loader function from apiRestaurant data */
 export async function loader({ params }) {
@@ -96,6 +97,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
