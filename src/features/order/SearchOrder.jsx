00707169d7920aaa1de99de309152d9b7ec831c8
Paul Fadayo <paulfadayo@yahoo.com>
2024-01-02 04:38:08 +0100

    chore: :lipstick: search order placeholder ui for mobile view

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
 
   function handleSubmit(e) {
@@ -10,7 +10,7 @@ function SearchOrder() {
 
     if (!query) return;
     navigate(`/order/${query}`);
-    setQuery('');
+    setQuery("");
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -18,7 +18,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400  focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-[9px] placeholder:text-stone-500 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:placeholder:text-sm sm:focus:w-72"
       />
     </form>
   );
